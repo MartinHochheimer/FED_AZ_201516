16ebec5fe4ddf7ed105dc34eb51b1d56a83a5af5
Martin Hochheimer <43468442+MartinHochheimer@users.noreply.github.com>
2019-12-02 19:32:37 +0100

    added functional library for custom Bash functions

--- file-orga-funcs.sh
+++ file-orga-funcs.sh
@@ -3,16 +3,17 @@
 subdir-filecounter()
 {
     # give dir, subdir and sequence-nametag for files to count
-    DIR=$1
-    SUBDIR=$2
-    SEQUENCE=$3
+   DIR=$1
+   SUBDIR=$2
+   SEQUENCE=$3
 
     PATHLIST=${DIR}/${SUBDIR}*/${SEQUENCE}*
 
     for files in $PATHLIST; do
 	echo ${PWD}/$files
-	FILENUMS=`ls -a $files | wc -l` # bash does not seem to recognize my aliases in own functions ... maybe look into dot sourcing in my bash aliases or some more elegant way ...
-	echo "`expr $FILENUMS - 2` files." # -2 is for the directory references in ls command
+	NUMS=`ls -a $files | wc -l` # bash does not seem to recognize my aliases in own functions ... maybe look into dot sourcing in my bash aliases or some more elegant way ...
+	FILENUMS=`expr $NUMS - 2` # get rid of directory references in ls command 
+	echo "$FILENUMS files."
     done
     
 }
--- filecount_subdirs.sh
+++ filecount_subdirs.sh
@@ -2,8 +2,5 @@
 # Interaction with subdir-filecounter
 . /media/storage/data/FED-project_UoA_data/file-orga-funcs.sh
 
-echo "Please enter dir, subdir and sequence abbrev(\*) to count files in:"
-
-read x y z
-
-subdir-filecounter $x $y $z
+# execute function
+subdir-filecounter $1 $2 $3
