2b795215b1243b4f6af0075d5c47ace2cb5a9b95
Martin Hochheimer <43468442+MartinHochheimer@users.noreply.github.com>
2021-02-26 17:14:46 +0100

    Finished the editing script tp append the fsf files.

--- data_funclib_scripts_exec/edit_fsf_files.sh
+++ data_funclib_scripts_exec/edit_fsf_files.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+# Execute all of the following out of the VBM directory!
 
 # collect all fsf files into a list
-FSF_FILES=$(find /media/storage/data/FED-project_UoA_data/fMRI/ -maxdepth 4 -type f -regextype posix-egrep -regex ".*FED007.*run0.fsf.*")
+FSF_FILES=$(find ./fMRI/ -maxdepth 4 -type f -regextype posix-egrep -regex ".*FED007.*run0.fsf")
 
 # find the yet suboptimal parts of the fsf file and edit them as required
-for file in FSF_FILES; do
+for file in $FSF_FILES; do
+    # print file
     echo $file
-    sed -n -e "/outputdir/s/run0//p" $file
+    # split string by "/" and create replacement variables
+    # need to escape forward slashes, or sed won't take it
+    OUTPUT_DIRECTORY="$(cut -d'/' -f1-5 <<<$file --output-delimiter="\/")\/FEAT"
+    HIGHRES_IMG="$(cut -d'/' -f1-3 <<<$file --output-delimiter="\/")\/T1_BETBf35.nii"
+    NO=0
+    echo $OUTPUT_DIRECTORY
+    echo $HIGHRES_IMG
+    echo $NO
+    # replace lines in fsf file with desired input to FEAT analysis
+    sed -in -e "/outputdir/s/run0/$OUTPUT_DIRECTORY/" -e "/temphp_yn/s/1/$NO/" -e '/highres_files/s/""//'\
+    -e "/highres_files/s/$/$HIGHRES_IMG/" -e '/highres_files/s/.\//".\//' -e '/highres_files/s/ii/ii"/'\
+    -e '/feat_files/s/"\//".\//' $file
 done
