a15a2939b4e25c9e2145c9cd865749da27307b41
Martin Hochheimer <43468442+MartinHochheimer@users.noreply.github.com>
2021-03-06 20:01:32 +0100

    Added code to edit/infer unavailable parameters from json-extraction.

--- data_funclib_scripts_exec/edit_fsf_files.sh
+++ data_funclib_scripts_exec/edit_fsf_files.sh
@@ -9,15 +9,26 @@ for file in $FSF_FILES; do
     # print file
     echo $file
     # split string by "/" and create replacement variables
-    # need to escape forward slashes, or sed won't take it
+    # need to escape forward slashes, sed will interpret and not print them
     OUTPUT_DIRECTORY="$(cut -d'/' -f1-5 <<<$file --output-delimiter="\/")\/FEAT"
     HIGHRES_IMG="$(cut -d'/' -f1-3 <<<$file --output-delimiter="\/")\/T1_BETBf35.nii"
+    FEAT_FILE="$(cut -d'/' -f1-3 <<<$file --output-delimiter="\/")\/"
     NO=0
-    echo $OUTPUT_DIRECTORY
-    echo $HIGHRES_IMG
-    echo $NO
+#    echo $OUTPUT_DIRECTORY
+#    echo $HIGHRES_IMG
+#    echo $FEAT_FILE
+#    echo $NO
     # replace lines in fsf file with desired input to FEAT analysis
-    sed -in -e "/outputdir/s/run0/$OUTPUT_DIRECTORY/" -e "/temphp_yn/s/1/$NO/" -e '/highres_files/s/""//'\
-    -e "/highres_files/s/$/$HIGHRES_IMG/" -e '/highres_files/s/.\//".\//' -e '/highres_files/s/ii/ii"/'\
-    -e '/feat_files/s/"\//".\//' $file
+    # the builder for the fsf file produces an eror in the path of the feat_files. Correct it.
+    sed -in -e "/outputdir/s/run0/$OUTPUT_DIRECTORY/"\
+           -e "/temphp_yn/s/1/$NO/"\
+           -e '/highres_files/s/""//' -e "/highres_files/s/$/$HIGHRES_IMG/" -e '/highres_files/s/.\//".\//' -e '/highres_files/s/ii/ii"/'\
+           -e "/feat_files/s/\/fMRI.*ns\//$FEAT_FILE/"\
+           -e "s/\.\/fMRI/\/fMRI/g" $file
+done
+
+
+# if needed, change relative to absolute paths
+for file in $FSF_FILES; do
+    sed -in -e 's/\/fMRI/\/media\/storage\/data\/FED-project_UoA_data\/fMRI/g' $file
 done
