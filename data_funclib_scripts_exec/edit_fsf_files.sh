c4f07766a6f3f7e094b455447b2f2f99549363a8
Martin Hochheimer <43468442+MartinHochheimer@users.noreply.github.com>
2021-03-29 13:02:40 +0200

    selected ROIs based on Smith et al 2009 and Koster 2005 etc.; finished template creation for group analysis

--- data_funclib_scripts_exec/edit_fsf_files.sh
+++ data_funclib_scripts_exec/edit_fsf_files.sh
@@ -19,18 +19,25 @@ for file in $FSF_FILES; do
     # input correct output directory
     # no temporal highpass filtering - already did that
     # set smoothing kernel to 5mm, but set smoothing to 0 in the next line - already did that
+    # set registration to standard space right, but leave switched-off (FSL readability^^)
     # set within subject registration to BBR
     # turn threshold value to 0 (median intensity normalised to 10000 and twice-assured)
-    # switch to only performing stats and not post-stats
+    # switch to only performing stats and not post-stats; switch off posstats; switch off timeseries plots
     # insert highres for plotting statistics
     # inserting the correct feat analysis file and path
     # take away the . to switch to absolute paths (see below)
     sed -in -e "/outputdir/s/run0/$OUTPUT_DIRECTORY/"\
            -e "/temphp_yn/s/1/0/"\
-           -e "/fmri(smooth)/s/0/5/" -e "/.*fmri(smooth).*/a set fmri(smooth_yn) 0"\
+           -e "/fmri(smooth)/s/0/5/"\
+           -e "/fmri(regstandard)/s/MNI152/\/media\/storage\/data\/fsl\/data\/standard\/MNI152_T1_2mm_brain/"\
+           -e "/fmri(regstandard_dof)/s/0/12/"\
+           -e "/fmri(regstandard_search)/s/90/180/"\
+           -e "/fmri(regstandard_nonlinear_yn)/s/0/1/"\
            -e "/highres_dof/s/6/BBR/"\
            -e "/fmri(thresh)/s/3/0/"\
-           -e "/fmri(analysis)/s/6/5/"\
+           -e "/fmri(analysis)/s/6/2/"\
+           -e "/fmri(poststats_yn)/s/1/0/"\
+           -e "/fmri(tsplot_yn)/s/1/0/"\
            -e '/highres_files/s/""//' -e "/highres_files/s/$/$HIGHRES_IMG/" -e '/highres_files/s/.\//".\//' -e '/highres_files/s/ii/ii"/'\
            -e "/feat_files/s/\/fMRI.*ns\//$FEAT_FILE/"\
            -e "s/\.\/fMRI/\/fMRI/g" $file
